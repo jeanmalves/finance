c63c8773696f5f64de760ff6a0b39b190e40b2e0
Jeam Alves <jeanmalves@gmail.com>
2018-11-07 01:16:55 -0200

    [controller] - Removido middleware e adicionado ao grupo de rota

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/user/edit', 'UserController@edit')->name('userEdit');
+    Route::get('/settings/edit', 'SettingsController@edit')->name('settingsEdit');
+});
+
