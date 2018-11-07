65ab0a066d7da38613ec160e3b2541944c7dd60b
Jeam Alves <jeanmalves@gmail.com>
2018-11-07 01:17:19 -0200

    [view] - Seetings e user

---
+++ resources/views/settings/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+
+
+@section('content_header')
+    <h1>Edit App</h1>
+@stop
+
+@section('content')
+    <p>{{ $user }}</p>
+@stop
---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+
+
+@section('content_header')
+    <h1>Edit user</h1>
+@stop
+
+@section('content')
+    <p>{{ $user }}</p>
+@stop
