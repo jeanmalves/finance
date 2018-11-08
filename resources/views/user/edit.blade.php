c42d318d238b40a964aa64287aaebd726d614bcb
Jeam Alves <jeanmalves@gmail.com>
2018-11-08 01:25:29 -0200

    [Layout] - Adicionado layout edição user.

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -3,9 +3,25 @@
 
 
 @section('content_header')
-    <h1>Edit user</h1>
+    <h1>
+        <i class="fa fa-user"></i>
+        Perfil do Usuário
+        <small>Editar perfil</small>
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
+    <li class="active">Perfil</li>
+  </ol>
 @stop
 
 @section('content')
-    <p>{{ $user }}</p>
+    <div class="box box-default color-palette-box">
+        <div class="box-header with-border"></div>
+        <div class="box-body">
+        <div class="row">
+
+        <!-- /.row -->
+        </div>
+        <!-- /.box-body -->
+    </div>
 @stop
