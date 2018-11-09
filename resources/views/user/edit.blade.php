ff2856e2606a13bc21ca2d221f884d9087387a5a
Jeam Alves <jeanmalves@gmail.com>
2018-11-09 00:19:26 -0200

    [View] - Adicionado form user

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-
+@section('title', 'Perfil')
 
 @section('content_header')
     <h1>
@@ -15,11 +15,35 @@
 @stop
 
 @section('content')
-    <div class="box box-default color-palette-box">
+    <div class="box box-default">
         <div class="box-header with-border"></div>
         <div class="box-body">
         <div class="row">
+            <div class="col-md-6">
+                <form class="form-horizontal">
+                    <div class="box-body">
+                      <div class="form-group">
+                        <label for="inputName" class="col-sm-2 control-label">Nome</label>
 
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputName" placeholder="Nome">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassword" class="col-sm-2 control-label">Password</label>
+
+                        <div class="col-sm-10">
+                          <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                        </div>
+                      </div>
+                    </div>
+                    <!-- /.box-body -->
+                    <div class="box-footer">
+                      <button type="submit" class="btn btn-primary pull-right">Editar</button>
+                    </div>
+                    <!-- /.box-footer -->
+                  </form>
+            </div>
         <!-- /.row -->
         </div>
         <!-- /.box-body -->
