c11c6fd631dd7908f7e14d6993ded0ae53e0b73b
Jeam Alves <jeanmalves@gmail.com>
2018-11-11 23:22:09 -0200

    [View] - Lista de categorias

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,16 @@
+@extends('adminlte::page')
+
+@section('title', 'Categorias de lançamento')
+
+@section('content_header')
+    <h1>
+        <i class="fa fa-user"></i>
+        Categorias de Lançamento
+        <small>Lista de categorias</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="/"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="/categories"> Categorias de lançamento</a></li>
+        <li class="active">Lista de categorias</li>
+    </ol>
+@stop
