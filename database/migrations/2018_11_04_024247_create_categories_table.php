8cbaa7fb83bbf656530736d499dc6858238bfba7
Jeam Alves <jeanmalves@gmail.com>
2018-11-11 22:55:26 -0200

    [Migration] - Adicionado campo type

--- database/migrations/2018_11_04_024247_create_categories_table.php
+++ database/migrations/2018_11_04_024247_create_categories_table.php
@@ -16,6 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->enum('type', ['C', 'D']);
             $table->integer('user_id')->unsigned();
             $table->timestamps();
 
