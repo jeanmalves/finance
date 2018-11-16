7ffba89debdbb28d8dcffc8c14cd1e979c100bf0
Jeam Alves <jeanmalves@gmail.com>
2018-11-16 00:22:16 -0200

    [View] - Adicionado modal para categorias de lançamento

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,4 +1,4 @@
-@extends('adminlte::page')
+@extends('layouts.app')
 
 @section('title', 'Categorias de lançamento')
 
@@ -14,3 +14,122 @@
         <li class="active">Lista de categorias</li>
     </ol>
 @stop
+
+@section('content')
+    <div class="alert alert-success alert-dismissible">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4>
+            <i class="icon fa fa-check"></i>
+        </h4>
+    </div>
+    <div class="box box-default color-palette-box">
+        <div class="box-header with-border">
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">
+                <i class="fa fa-database"></i>
+                Adicionar nova categoria
+            </button>
+            <div class="modal fade" id="modal-default">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <form id="category-form" action="{{ url('/categories') }}" class="form-horizontal">
+                            @csrf
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span></button>
+                                <h4 class="modal-title">Adicionar Nova Categoria de Lançamento</h4>
+                            </div>
+                            <div class="modal-body">
+                                <div class="box-body">
+                                    <div id="fg-category-name" class="form-group">
+                                        <label for="category-name">Categoria</label>
+                                        <input type="name" class="form-control" id="category-name" placeholder="Digite a categoria">
+                                    </div>
+                                    <div id="fg-category-type" class="form-group">
+                                        <label for="category-type">Tipo da categoria</label>
+                                        <select name="type" id="category-type" class="form-control">
+                                            <option value="C">Entrada</option>
+                                            <option value="D">Saída</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                                <button type="submit" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </form>
+                    </div>
+                    <!-- /.modal-content -->
+                </div>
+                <!-- /.modal-dialog -->
+            </div>
+            <!-- /.modal -->
+        </div>
+        <div class="box-body">
+            <div class="row">
+
+                <!-- /.row -->
+            </div>
+            <!-- /.box-body -->
+    </div>
+@stop
+@push('js')
+<script type="text/javascript">
+    $('.alert-success').hide();
+    $('#modal-default').on('hidden.bs.modal', function(e){
+        resetErrorForm();
+    });
+
+    $('#category-form').submit((e) => {
+        e.preventDefault();
+        resetErrorForm();
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $.ajax({
+            url: $('#category-form').attr('action'),
+            method: 'POST',
+            data: {
+                name: $('#category-name').val(),
+                type: $('#category-type').children('option:selected').val()
+            },
+            success: (result) => {
+                if (result.errors) {
+                    if (result.errors.name) {
+                        $('#category-form').find('#fg-category-name').addClass('has-error');
+                        $('#category-form').find('#fg-category-name').append(
+                            '<span class="help-block">' +
+                            '    <strong>' + result.errors.name + '</strong>' +
+                            '</span>');
+                    }
+
+                    if (result.errors.type) {
+                        $('#category-form').find('#fg-category-type').addClass('has-error');
+                        $('#category-form').find('#fg-category-type').append(
+                            '<span class="help-block">' +
+                            '    <strong>' + result.errors.type + '</strong>' +
+                            '</span>');
+                    }
+                }
+
+                $('#modal-default').modal('hide');
+                $('.alert-success').show();
+            },
+            error: (err) => {
+                console.log(err);
+            }
+        });
+    });
+
+    function resetErrorForm() {
+        $('#category-form').find('#fg-category-name').removeClass('has-error');
+        $('#category-form').find('#fg-category-type').removeClass('has-error');
+        $('#category-form').find('.help-block').remove();
+
+    }
+</script>
+@endpush
