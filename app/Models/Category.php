b0c643ac1752a87be831f9ef33d89fa1ab4d2a52
Jeam Alves <jeanmalves@gmail.com>
2018-11-16 19:56:13 -0200

    [Model] - Adicionado e attr rel. do usuario

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     * @var array
+     */
+    protected $fillable = ['name', 'type', ];
+
+    /**
+     * Get the user that owns a category
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
